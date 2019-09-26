b36f9c91d503417bf195d7f914e49152b5765359
Platon Bibik <bibik.platon@yandex.ru>
2019-09-26 12:15:15 +0200

    add formula

--- jsh.rb
+++ jsh.rb
@@ -5,11 +5,8 @@ class Jsh < Formula
   sha256 "a2af8bc815f866405a91ddfd9c5be4c989e1ff4f1be5d70923f98bb05245f178"
   version "0.1.0"
 
-  # depends_on "cmake" => :build
-
   def install
-    system "mv ./jsh /usr/local/bin"
-    system "chmod +x /usr/local/bin/jsh"
+    bin.install "jsh"
   end
 
   test do
