3d78733e0e7b38d52bdb4318077bd7744d3105ed
Platon Bibik <bibik.platon@yandex.ru>
2019-09-26 12:16:36 +0200

    fixme

--- jsh.rb
+++ jsh.rb
@@ -1,6 +1,7 @@
 class Jsh < Formula
   desc ""
   homepage ""
+  # FIXME: link to github release asset
   url "https://jsh-release.s3-eu-west-1.amazonaws.com/ki-hacks-jsh-release.tar.gz"
   sha256 "a2af8bc815f866405a91ddfd9c5be4c989e1ff4f1be5d70923f98bb05245f178"
   version "0.1.0"
