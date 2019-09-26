80fbaad3046e6ab4dfb009d5127fa7b6be24258e
Platon Bibik <bibik.platon@yandex.ru>
2019-09-26 10:45:24 +0200

    add formula

--- jsh.rb
+++ jsh.rb
@@ -1,8 +1,7 @@
 class Jsh < Formula
   desc ""
   homepage ""
-  url "https://example.com/foo-0.1.tar.gz"
-  sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+  url "git@github.com:KI-labs/ki-hacks-jsh.git"
 
   # depends_on "cmake" => :build
 
